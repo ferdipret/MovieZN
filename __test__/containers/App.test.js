71d4d6a60413c2b2557a7cc0de5209738e89304f
Ferdi <ferdinand.pretorius@strongbyte.com>
2018-04-01 18:37:44 +0100

    Added more test utils and input value change tests

--- __test__/containers/App.test.js
+++ __test__/containers/App.test.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { mount, find, simulate } from 'enzyme'
 
 import App from 'containers/App'
 
-// We should make sure the app doesn't have any problems
-// on the initial render
 describe('App', () => {
   test('should render correctly', () => {
     const rendered = renderer.create(<App />)
     expect(rendered.toJSON()).toMatchSnapshot()
+  })
+})
+
+describe('App/Input', () => {
+  test('should update state when value change', () => {
+    const wrapper = mount(<App />)
+    const input = wrapper.find('.search-input')
+
+    input.simulate('change', { target: { value: 'New Value' } })
+
+    expect(wrapper.state().searchInputValue).toBe('New Value')
   })
 })
