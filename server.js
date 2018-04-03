043c4c0461750e37eb25db5fa67edfea2443d6b7
Ferdi <ferdinandpretorius@gmail.com>
2018-04-03 23:00:15 +0100

    Setup a basic server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+// Create a new instance of express
+const app = express();
+
+// When we run the app on a server, we might want to set the port number through
+// an environment variable otherwise it will be `3000`
+const PORT = process.env.PORT || 3000;
+
+// Here we're just making sure that if a user tries to access the app through
+// a secure protocol, we're redirecting to http://
+app.use(
+  (req, res, next) =>
+    req.headers["x-forward-proto"] === "https"
+      ? res.redirect(`http://${req.hostname}${req.url}`)
+      : next()
+);
+
+// Setup our public directory
+app.use(express.static("public"));
+
+// Start the app and listen on port process.env.PORT || 3000
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
