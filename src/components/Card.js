d45643a2f72b6f2e5234d6bfbfd403bbe0860f43
Ferdi <ferdinandpretorius@gmail.com>
2018-04-03 22:51:50 +0100

    Add Card component and make basic call to API when app mounts

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Grid from 'components/Grid'
+
+const Card = ({ className, children }) => (
+  <Grid item xs={{ span: 12, offset: 0 }}>
+    <div className={className}>{children}</div>
+  </Grid>
+)
+
+export default styled(Card)``
